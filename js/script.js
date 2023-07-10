26bb2e8bbc7befda3d0b3d7c313b4a737b318cd3
Abdiel <abdiel.dsem@gmail.com>
2023-07-10 18:36:51 -0400

    commit 13

--- js/script.js
+++ js/script.js
@@ -220,3 +220,18 @@ Floats with decimal part non equal to zero are considered UNeven for this kata.*
 function testEven(n) {
   return n % 2 === 0;
 }
+// Question13 | ky8 | Is it even?
+
+/*In this Kata we are passing a number (n) into a function.
+
+Your code will determine if the number passed is even (or not).
+
+The function needs to return either a true or false.
+
+Numbers may be positive or negative, integers or floats.
+
+Floats with decimal part non equal to zero are considered UNeven for this kata.*/
+
+function testEven(n) {
+  return n % 2 === 0;
+}
